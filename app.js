2be92378740be80d791b39b7181d13ef3e08b218
Raghu <vamsiraghu0024@gmail.com>
2022-12-30 14:55:50 +0000

    creation APIs for cricket team

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const dbPath = path.join(__dirname, "cricketTeam.db");
+const app = express();
+app.use(express.json());
+let db = null;
+
+const initilizeDatabaseAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("the server running at http://localhost:3000");
+    });
+  } catch (error) {
+    console.log(`database error:${error.message}`);
+    process.exit(1);
+  }
+};
+
+initilizeDatabaseAndServer();
+
+//Get All Players
+app.get("/players/", async (request, response) => {
+  const getAllPlayersQuery = `
+    SELECT
+     * 
+    FROM
+    cricket_team
+    ORDER BY playerId;`;
+  const playersArray = await db.all(getAllPlayersQuery);
+  response.send(playersArray);
+});
